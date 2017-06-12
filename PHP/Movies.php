7016dead4d89c80dd098a2e60c406e12a1d6b34d
Dionisis <dionisis_1995@hotmail.com>
2017-06-12 15:54:03 +0300

    Movies loading succesfully loading

--- PHP/Movies.php
+++ PHP/Movies.php
@@ -12,8 +12,10 @@
 </html>
 
 <?php
+	include 'DBconnect.php';
+	include 'Movie.php';
 	$movies=array();
-	$sql = "SELECT * FROM Movies";
+	$sql = "SELECT * FROM Movie";
 	$result = $mysqli->query($sql);
 	if ($result->num_rows > 0) {
 		while($row = $result->fetch_assoc()) { // looping through Movies
@@ -26,12 +28,12 @@
 			$rating=$row["rating"]; //TODO check wether rating isset (or something)
 			
 
-			$sql_genre="select genre_description from genre, movie_genre where Genre_id=Genre.id and movie_id=".$movieID ."as genre";
-			$result_choice = $mysqli->query($sql_choice);
+			$sql_genre="select genre_description as genre from genre, movie_genre where Genre_id=Genre.id and movie_id=".$movieID ;
+			$result_choice = $mysqli->query($sql_genre);
 			while($row_choice = $result_choice->fetch_assoc()) { //Looping through Genres
 				array_push($genres,$row_choice["genre"]);
 			}
-			$movie=new Movie($movieID,$title,$releaseYear,$genres,$description,$rating);
+			$movie=new Movie($movieID,$title,$releaseYear,$genres,$image,$description,$rating);
 			array_push($movies,$movie);
 		}
 
@@ -39,12 +41,16 @@
 		foreach($movies as $movie) {
 		
 		echo  $movie->getTitle() ."<br> ";
+		echo  $movie->getReleaseYear() ."<br> ";
+		echo  $movie->getDescription() ."<br> ";
+		echo "<img src=\"".$movie->getImage()."\"  >";
+
+
 		foreach($movie->getGenre() as $genre) {
 			echo $genre."<br>";
 		}
 		echo "<br>";
 
-		$i++;
 		}
 		
 		
