d57669fd1b5a49309514d2f190658f37bdbb8aac
1JBourne <n.papadogoulas@gmail.com>
2017-06-13 17:57:16 +0300

    JS OK

--- JS/MoviesScript.js
+++ JS/MoviesScript.js
@@ -1,10 +1,16 @@
 function groupBy(genre) {
+	var reset = document.querySelectorAll('div');
+	for (var i = 0; i < reset.length; i++) {
+		reset[i].style.display='';
+	}
 	if (genre=="") return; // please select - possibly you want something else here
 	// alert("it is Working");
 	genre = genre.toLowerCase();
 
-	console.log(genre);
-	var elements = document.querySelector('div :not(genre)');
+	var elements = document.querySelectorAll('div:not(.' + genre + ')');
 	console.log(elements);
+	for (var i = 0; i < elements.length; i++) {
+		elements[i].style.display='none';
+	}
 	// divOne.style.display='none';
 } 
